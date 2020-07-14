318d870acbd82dbef10367eb7ae6ed428c0f8e60
marcinklose <mklose12@gmail.com>
2020-07-14 13:44:50 +0200

    Add CORS

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_restplus import Api
 from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__, static_folder='static')
+app.config['CORS_HEADERS'] = 'Content-Type'
+cors = CORS(app)
 ma = Marshmallow(app)
 api = Api(app)
